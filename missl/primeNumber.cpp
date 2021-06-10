abdd5fe8b81614c112fbdfd32aa5281c185d0c39
DESKTOP-GI8EU5B\progr <valomanas99@gmail.com>
2021-06-10 01:00:44 +0100

    added folder

--- missl/primeNumber.cpp
+++ missl/primeNumber.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
-
+ 
 int main()
 {
     int n;
     cin >> n;
 
-    for(int i=n; i>=1; i--)
+    for(int i=2; i<n; i++)
     {
-        for(int j=1; j<=i; j++)
+        if(n%i == 0)
         {
-            cout << "*";
+            cout << "Non-prime" << endl;
+            break;
         }
-        cout << endl;
     }
 }
