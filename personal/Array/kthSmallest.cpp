92b29cbac0c566da6ce9080975fbce922858db98
DESKTOP-GI8EU5B\progr <valomanas99@gmail.com>
2021-06-12 00:37:06 +0100

    kth Smallest Number in the array

---
+++ personal/Array/kthSmallest.cpp
@@ -0,0 +1,23 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int k;
+    cin >> k;
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    sort(a, a + n);
+
+    cout << "MAX: " << a[k-1];
+
+
+}
