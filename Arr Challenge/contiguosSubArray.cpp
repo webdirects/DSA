7cae53ac248a978dbebbfb1ddc6f451eb87046f7
DESKTOP-GI8EU5B\progr <valomanas99@gmail.com>
2021-06-11 23:39:38 +0100

    sum of highest contiguous subarray

--- Arr Challenge/contiguosSubArray.cpp
+++ Arr Challenge/contiguosSubArray.cpp
@@ -12,14 +12,19 @@ int main()
         cin >> a[i];
     }
 
+    int maxSum = INT_MIN;
     for(int i=0; i<n; i++)
     {
         for(int j=i; j<n; j++)
         {
+            int sum = 0;
             for(int k=i; k<=j; k++)
             {
-                cout << a[k] << " ";
+                sum += a[k];
+                // cout << a[k] << " ";
             } cout << endl;
+            maxSum = max(maxSum, sum);
         }
     }
+    cout << maxSum << endl;
 }
