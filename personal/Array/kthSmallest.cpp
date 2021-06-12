65a9fc99c0a14f5654b9c59b342a6acd62542a35
DESKTOP-GI8EU5B\progr <valomanas99@gmail.com>
2021-06-12 09:37:53 +0100

    union

--- personal/Array/kthSmallest.cpp
+++ personal/Array/kthSmallest.cpp
@@ -1,23 +1,42 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+void getunion(int a[], int n, int b[], int m)
+{
+    set<int> s;
+    
+    for(int i=0; i<n; i++)
+    {
+        s.insert(a[i]);
+    }
+    for(int j=0; j<m; j++)
+    {
+        s.insert(b[j]);
+    }
+
+    cout << "Union is: " << endl;
+    for(auto itr=s.begin(); itr != s.end(); itr++)
+    {
+        cout << *itr << " ";
+    }
+}
+
 int main()
 {
-    int n;
-    cin >> n;
-    int k;
-    cin >> k;
+    int n,m;
+    cin >> n >> m;
 
-    int a[n];
-
-    for (int i = 0; i < n; i++)
+    int a[n], b[m];
+    
+    for(int i=0; i<n; i++)
     {
         cin >> a[i];
     }
 
-    sort(a, a + n);
+    for(int i=0; i<m; i++)
+    {
+        cin >> b[i];
+    }
 
-    cout << "MAX: " << a[k-1];
-
-
+    getunion(a, n, b, m);
 }
